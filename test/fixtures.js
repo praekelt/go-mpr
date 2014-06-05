c60445febccaa2eb70d72de0318c060c7cf2e150
Merada Richter <merada.richter@gmail.com>
2014-06-05 15:18:44 +0200

    Removes unused fixture.

--- test/fixtures.js
+++ test/fixtures.js
@@ -222,37 +222,5 @@ module.exports = function() {
                 }
             ]
         }
-    },
-    {
-        "request": {
-            "method": "POST",
-            "url": "http://httpbin.org/post",
-            "data": {
-                "message": "hello world!"
-            }
-        },
-        "response": {
-            "code": 200,
-            "data": {
-                "data": "{\"message\":\"hello world!\"}",
-                "form": {},
-                "origin": "192.168.0.23",
-                "url": "http://httpbin.org/post",
-                "args": {},
-                "files": {},
-                "headers": {
-                    "Content-Type": "application/json",
-                    "Host": "httpbin.org",
-                    "Cookie": "",
-                    "X-Request-Id": "a3b50df8-d3d0-40cb-ab33-bcada3e7d012",
-                    "Content-Length": "26",
-                    "Accept-Encoding": "gzip, deflate",
-                    "Connection": "close"
-                },
-                "json": {
-                    "message": "hello world!"
-                }
-            }
-        }
     }];
 };
